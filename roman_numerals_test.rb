15b83de565386704e574eb9e56591451ca4db1d7
Josh Varney <joshcainvarney@gmail.com>
2018-10-16 16:35:37 -0400

    all tests passed can convert roman to arabic and back again

--- roman_numerals_test.rb
+++ roman_numerals_test.rb
@@ -36,11 +36,11 @@ class Roman_numerals_test < Minitest::Test
   #   assert_equal(["X", "I"], arabic_converter("XI"))
   #   assert_equal(5, arabic_converter("MXIIV").count)
   # end
-  def test_that_converter_splits_into_array
-    assert_equal(["XC", 0, "IV", 0, "IX", 0], arabic_converter("XCIVIX"))
-    assert_equal(["X", "I", "IX", 0], arabic_converter("XIIX"))
-    assert_equal(["M", "X", "I", "IV", 0], arabic_converter("MXIIV"))
-  end
+  # def test_that_converter_splits_into_array
+  #   assert_equal(["XC", 0, "IV", 0, "IX", 0], arabic_converter("XCIVIX"))
+  #   assert_equal(["X", "I", "IX", 0], arabic_converter("XIIX"))
+  #   assert_equal(["M", "X", "I", "IV", 0], arabic_converter("MXIIV"))
+  # end
    def test_that_can_convert_numbers_in_the_teens
     assert_equal(11, arabic_converter("XI"))
     assert_equal(14, arabic_converter("XIV"))
@@ -48,11 +48,11 @@ class Roman_numerals_test < Minitest::Test
     assert_equal(16, arabic_converter("XVI"))
     assert_equal(18, arabic_converter("XVIII"))
   end
-  # def test_that_works_on_bigger_numbers
-  #   assert_equal("MCMLXXXIX", roman_converter(1989))
-  #   assert_equal("MLXVI", roman_converter(1066))
-  #   assert_equal("XLIX", roman_converter(49))
-  #   assert_equal("CMXCIX", roman_converter(999))
-  #   assert_equal("DCLXVI", roman_converter(666))
-  # end 
+  def test_that_can_convert_numbers_in_the_teens
+    assert_equal(1989, arabic_converter("MCMLXXXIX"))
+    assert_equal(1066, arabic_converter("MLXVI"))
+    assert_equal(49, arabic_converter("XLIX"))
+    assert_equal(999, arabic_converter("CMXCIX"))
+    assert_equal(666, arabic_converter("DCLXVI"))
+  end
 end  
