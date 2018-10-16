f3a89fc18f9c63596b075d5e0982b4ca2b2f755e
Josh Varney <joshcainvarney@gmail.com>
2018-10-16 14:20:44 -0400

    made test for converter in the teen range

--- roman_numerals_test.rb
+++ roman_numerals_test.rb
@@ -6,7 +6,7 @@ class Roman_numerals_test < Minitest::Test
     assert_equal(Hash, letters().class)
   end
   def test_that_hash_has_enough_elements_currently
-    assert_equal(7, letters().count)
+    assert_equal(5, letters().count)
   end
   # def test_that_roman_converter_returns_argument
   #   assert_equal(9, roman_converter(9))
@@ -14,4 +14,11 @@ class Roman_numerals_test < Minitest::Test
   def test_that_can_convert_9
     assert_equal("IX", roman_converter(9))
   end
+  def test_that_can_convert_numbers_in_the_teens
+    assert_equal("XI", roman_converter(11))
+    assert_equal("XIV", roman_converter(14))
+    assert_equal("XIX", roman_converter(19))
+    assert_equal("XVI", roman_converter(16))
+    assert_equal("XVIII", roman_converter(18))
+  end  
 end  
